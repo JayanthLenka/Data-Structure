d3cab6f2039b9135d8abe1aedde6b83bd748d53c
25A31A05IB <lenkajayanth29@gmail.com>
2026-02-21 15:48:41 +0530

    Program 2.cpp

--- Week 1/Program 2.cpp
+++ Week 1/Program 2.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+cout<<"enter a and b values:" ;
+cin>>a>>b;
+cout<<"value of a is : "<<a<<endl<<"value of b is :"<<b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"value of a is:"<<a<<endl<<"value of b is :"<<b;
+}
