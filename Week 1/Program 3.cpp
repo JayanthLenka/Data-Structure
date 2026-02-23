9f5f44f02895ea3735c358fb3774bc42b1c89050
25A31A05IB <lenkajayanth29@gmail.com>
2026-02-23 20:05:29 +0530

    Program 3.cpp

--- Week 1/Program 3.cpp
+++ Week 1/Program 3.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+char op;
+cout<<"enter the a and b values";
+cin>>a>>b;
+cout<<"enter the operator";
+cin>>op;
+switch(op)
+{
+case'+':cout<<"addition : "<<a+b;
+break;
+case'-' :cout<<"subtraction : "<<a-b;
+break;
+case'*' :cout<<"multiplication :"<<a*b;
+break;
+case'/':
+if(b==0)
+  cout<<"division not possible";
+else
+  cout<<"division"<<a/b;
+break;
+default:cout<<"invalid operator";
+}
+}
